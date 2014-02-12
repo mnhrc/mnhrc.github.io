32fc542af6c6f23c73c512901351fcaf0278746f
Jason Myers <jason@jasonamyers.com>
2014-02-12 11:12:56 -0600

    Changing back to primary worksheet
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- map_controller.js
+++ map_controller.js
@@ -5,7 +5,7 @@ var courses = [];
 // Ask for data from the spreadsheet.
 function startDataLoad(callback) {
     var spreadsheetKey = "0Asw-rVCOgjt8dFBndXdYTWVhaDJpaW5LbXl2QTliUWc";
-    var wsId = "2";
+    var wsId = "od6";
     var url = "http://cors.io/spreadsheets.google.com/feeds/list/" + spreadsheetKey + "/" + wsId + "/public/values?alt=json";
 
     $.getJSON(url, function (json) {
