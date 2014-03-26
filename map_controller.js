7885f7fe65db9dd1dd8d208c0bcf04780be7eb13
Jason Myers <jason@jasonamyers.com>
2014-03-26 07:42:19 -0500

    Changing map zoom
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- map_controller.js
+++ map_controller.js
@@ -237,7 +237,7 @@ function initialize() {
     // Create the map.
     map = new google.maps.Map(document.getElementById("map-canvas"), {
         center: new google.maps.LatLng(36.16, 273.215),
-        zoom: 12,
+        zoom: 11,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
     geocoder = new google.maps.Geocoder();
